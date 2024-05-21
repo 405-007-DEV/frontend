3f08d3a33dae51c45a1ae229dc53655477a63faa
sandwe <preengus@gmail.com>
2024-05-21 23:20:55 +0900

    fix: 스타일 충돌 처리

--- src/pages/UIPage/index.tsx
+++ src/pages/UIPage/index.tsx
@@ -2,8 +2,14 @@ import {Button} from "@/components/ui/button";
 
 export function UIPage() {
   return (
-    <div>
-      <Button disabled variant={"default"} className="w-full">
+    <div className="space-y-2">
+      <Button variant={"default"} className="w-full">
+        안녕
+      </Button>
+      <Button variant={"outline"} className="w-full">
+        안녕
+      </Button>
+      <Button disabled variant={"disabled"} className="w-full">
         안녕
       </Button>
     </div>
