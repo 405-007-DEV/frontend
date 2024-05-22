7a9a0a2cb903f8693025f8a997c307e4edbeb8be
sandwe <preengus@gmail.com>
2024-05-23 00:46:12 +0900

    feat: button 공통 스타일 w-full 추가

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none w-full',
   {
     variants: {
       variant: {
@@ -14,10 +14,10 @@ const buttonVariants = cva(
         disabled: 'border border-gr2 bg-gr1 text-gr3 b1_bold',
       },
       size: {
-        default: "h-56 px-4 py-2 rounded-md",
-        sm: "h-58 rounded-md px-3",
-        lg: "h-60 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: 'h-56 px-4 py-2 rounded-md',
+        sm: 'h-58 rounded-md px-3',
+        lg: 'h-60 rounded-md px-8',
+        icon: 'h-10 w-10',
       },
     },
     defaultVariants: {
