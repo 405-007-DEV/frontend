bbe86363f7ca1b85e53fe63bf6c2dff1ddeb2378
sandwe <preengus@gmail.com>
2024-05-23 00:53:53 +0900

    fix: padding 지정 요소 변경

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 export function Layout() {
   return (
-    <div className="flex flex-col h-screen px-20 box-border">
-      <div className="overflow-auto grow min-h-0">
+    <div className="flex flex-col h-screen box-border">
+      <div className="overflow-auto grow min-h-0 px-20">
         <Outlet />
       </div>
     </div>
