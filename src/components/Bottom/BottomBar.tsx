488fe677fa286577642bfc70f7ebe87a03c8fd54
sandwe <preengus@gmail.com>
2024-05-24 20:29:53 +0900

    fix: bottom bar 너비 수정

--- src/components/Bottom/BottomBar.tsx
+++ src/components/Bottom/BottomBar.tsx
@@ -4,7 +4,7 @@ interface BottomBarProps extends PropsWithChildren {}
 
 export function BottomBar({ children }: BottomBarProps) {
   return (
-    <div className="fixed bottom-0 w-full right-0 left-0 border-[1px] pt-26 pb-64 px-20 bg-accent-w">
+    <div className="fixed bottom-0 w-full right-0 left-0 pt-26 pb-64 px-20 bg-accent-w max-w-[500px] mx-auto">
       {children}
     </div>
   );
